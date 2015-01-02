73476d29654311653c8e02db0d41908bed5ad8cc
Nicholas Reed <nicholas.a.reed@gmail.com>
2015-01-01 16:44:07 -0800

    cleared

--- src/help.js
+++ src/help.js
@@ -4,7 +4,7 @@ define(function(require) {
 
     // Extras
     // var _ = require('underscore');
-    // var Utils = require('utils');
+    var Utils = require('utils');
 
     return {
         launch: function(key){
@@ -15,29 +15,6 @@ define(function(require) {
 
 
                 var opts = {
-
-                    todo_invoices : {
-                        type: 'popover',
-                        title: 'Switch to Invoices',
-                        body: 'Tapping this will switch to a view of Invoices'
-                    },
-
-                    todo_circle_checkmark : {
-                        type: 'popover',
-                        title: 'Cycle between Todos',
-                        body: 'Tapping this will switch between 3 todo types:<br />' + 
-                              '<p><i class="icon ion-ios7-circle-outline"></i> Todo</p>' + 
-                              '<p><i class="icon ion-ios7-checkmark-outline"></i> Complete</p>' + 
-                              '<p><i class="icon ion-ios7-checkmark"></i> All</p>'
-                    },
-
-                    'PaymentSource/List/BankDetails' : {
-                        type: 'popover',
-                        title: 'Accepting Payments',
-                        body: "To accept payments, we'll need a few more details from you. " + 
-                              "Please email us at founders@thehandyapp.com (or use the feedback form from the Settings menu) and we'll get you set up!<br /><br />" +
-                              "We apologize for the inconvenience!"
-                    }
                     
                 };
 
